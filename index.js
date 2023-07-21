2909625305055caa462390ff90170b9de2cadd51
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-07-21 15:47:01 +1100

    132135555

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
 const TelegramBot = require('node-telegram-bot-api');
-
+const geolib = require('geolib');
 // Установите токен вашего бота, полученный от BotFather
 const token = '6313272133:AAFrgEoF308LVQVuLNbsT0q_FL4HuEbRFT4';
 
 // Создаем экземпляр бота
 const bot = new TelegramBot(token, { polling: true });
 
-// Обработчик команды /location
-bot.onText(/\/location/, async (msg) => {
+// Фиктивные данные о морях и проливах для примера
+const seasAndStraits = [
+  { name: 'Black Sea', latitude: 42.0, longitude: 36.0 },
+  { name: 'Mediterranean Sea', latitude: 35.0, longitude: 18.0 },
+  { name: 'Arctic Ocean', latitude: 83.0, longitude: 0.0 },
+  // другие моря и проливы
+];
+
+// Обработчик команды /start
+bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
 
-  // Запрашиваем разрешение на доступ к местоположению
-  const request = await bot.sendMessage(chatId, 'Пожалуйста, предоставьте доступ к вашему местоположению.', {
-    reply_markup: {
-      keyboard: [
-        [
-          {
-            text: 'Предоставить местоположение',
-            request_location: true,
-          },
-        ],
-      ],
-      one_time_keyboard: true,
-    },
-  });
+  bot.sendMessage(chatId, 'Привет! Отправьте свое местоположение, чтобы определить ближайшее море или пролив.');
+});
 
-  // Ожидаем ответа с местоположением
-  bot.onReplyToMessage(chatId, request.message_id, (location) => {
-    // Обработка полученных координат
-    const latitude = location.location.latitude;
-    const longitude = location.location.longitude;
+// Обработчик местоположения пользователя
+bot.onLocation((msg) => {
+  const chatId = msg.chat.id;
+  const latitude = msg.location.latitude;
+  const longitude = msg.location.longitude;
 
-    // Формирование ссылки на карту OpenStreetMap
-    const mapUrl = `https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}#map=12/${latitude}/${longitude}`;
+  // Находим ближайшее море или пролив
+  const nearestSeaOrStrait = geolib.findNearest({ latitude, longitude }, seasAndStraits);
 
-    // Отправляем сообщение с полученными координатами и ссылкой на карту OpenStreetMap
-    bot.sendMessage(chatId, `Ваши координаты: ${latitude}, ${longitude}`);
-    bot.sendMessage(chatId, mapUrl);
-  });
+  // Отправляем сообщение с ближайшим морем или проливом
+  if (nearestSeaOrStrait) {
+    bot.sendMessage(chatId, `Ближайшее море или пролив: ${nearestSeaOrStrait.name}`);
+  } else {
+    bot.sendMessage(chatId, 'Не удалось найти ближайшее море или пролив.');
+  }
 });
